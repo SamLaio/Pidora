ee5073ca6035c8664426e74a76488f6cac91056d
SamLaio <f85122@gmail.com>
2014-08-26 16:21:50 +0800

    V1

---
+++ zh.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+#install xrdp
+yum install gcc pam-devel openssl-devel xrdp -y
+
+#install web server
+yum install httpd mysql-server php php-devel php-mysql sqlite sqlite-devel -y
+#yum install apache2 php5 libapache2-mod-php5 -y
+#yum install mysql-server mysql-client php5-mysql -y
+
+#ntfs
+sudo apt-get install ntfs-3g -y
+
+#zh_tw
+yum install system-config-language -y
+yum install cjkuni-ukai-fonts cjkuni-uming-fonts taipeifonts wqy-bitmap-fonts wqy-microhei-fonts -y
+yum install scim scim-tables scim-tables-chinese scim-tables-chinese-extra scim-array scim-chewing -y
+echo  "/etc/sysconfig/i18n"
+echo "LANG=\"en_US.UTF-8\"->LANG=\"zh_TW.UTF-8\""
